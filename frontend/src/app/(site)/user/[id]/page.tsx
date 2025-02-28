3a51e1cb166327960a4312d67a740051615f7f87
Vlad Dobrolyubov <cruzedvs@gmail.com>
2025-02-28 15:26:04 +0300

    feat(frontend user page): WIP. Started integrating with api

--- frontend/src/app/(site)/user/[id]/page.tsx
+++ frontend/src/app/(site)/user/[id]/page.tsx
@@ -1,14 +1,42 @@
-"use client"
-
 import Avatar from '@/components/Posts/Avatar';
 import styles from './styles.module.scss';
 import { Button, Card } from 'antd';
-import Post from '@/components/Posts/Post';
 import { EAvatar } from '@/variants/avatar';
 import { UserAddOutlined } from '@ant-design/icons';
 import PageTitle from '@/components/common/PageTitle';
+import PostsListWithFeedback from '@/components/Posts/PostsList';
+import { getUserPosts } from '@/api/posts';
+import { useEffect, useState } from 'react';
+import { TUser } from '@/types/user';
+import { useMessages } from 'helpers/hooks/useMessages';
+import { getUserById } from '@/api/users';
+import { requestWithReturn } from 'helpers/functions/requestWithReturn';
 
-const UserPage = () => {
+const UserPage = async ({ params }: { params: { id: string }}) => {
+  const [user, setUser] = useState<TUser | null>(null);
+  const [notFound, setNotFound] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const { errorMessage } = useMessages();
+  const { id } = await params;
+
+  const fetchUser = async (id: string) => {
+    const response = await requestWithReturn<string, TPost | null>(
+      getUserById,
+      id,
+      () => {
+        errorMessage("Ошибка при получении поста");
+        setNotFound(true);
+      },
+      setPost,
+      setIsLoading,
+    )
+    return response;
+  };
+  
+  useEffect(() => {
+    fetchUser(id);
+  }, [id])
+
   return (
     <article className={styles.userPage}>
       <PageTitle>Личная страница пользователя</PageTitle>
@@ -28,9 +56,7 @@ const UserPage = () => {
         <span className={styles.userPagePostsTitle}>
           Посты пользователя
         </span>
-        <Post />
-        <Post />
-        <Post />
+        <PostsListWithFeedback request={getUserPosts} />
       </div>
     </article>
   )
